9b5f46ab276e5bce5d08af38227429b622c11c32
taiseiue <taiseiue@wsnet.jp>
2025-08-07 19:22:02 +0900

    位置をとれるようにした

--- Curiosity/Curiosity/Platforms/Android/MainApplication.cs
+++ Curiosity/Curiosity/Platforms/Android/MainApplication.cs
@@ -1,14 +1,6 @@
 ﻿using Android.App;
 using Android.Runtime;
 
-[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
-[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
-[assembly: UsesFeature("android.hardware.location", Required = false)]
-[assembly: UsesFeature("android.hardware.location.gps", Required = false)]
-[assembly: UsesFeature("android.hardware.location.network", Required = false)]
-// Background location permission only needed if your app requires it
-// [assembly: UsesPermission(Android.Manifest.Permission.AccessBackgroundLocation)]
-
 namespace Curiosity;
 
 [Application]
