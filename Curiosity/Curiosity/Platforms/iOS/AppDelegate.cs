46347793dc7979c3c1efe93a5e0f97f97c29f43b
taiseiue <taiseiue@wsnet.jp>
2025-08-07 14:27:06 +0900

    Project initialize

---
+++ Curiosity/Curiosity/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Curiosity;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
