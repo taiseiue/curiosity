747d288e2b804f07fd9dfa1ae2158fb4c4183f56
taiseiue <taiseiue@wsnet.jp>
2025-08-22 04:02:51 +0900

    とりあえず情報のうけわたしができる

---
+++ Curiosity/Curiosity/Command.cs
@@ -0,0 +1,16 @@
+public enum Command
+{
+    Stop = 0,
+    Forward = 1,
+    ForwardLeft = 2,
+    ForwardRight = 3,
+    Backward = 4,
+    BackwardLeft = 5,
+    BackwardRight = 6,
+    Left = 7,
+    Right = 8,
+    TurnLeft = 9,
+    TurnRight = 10,
+    Reset = 11,
+    Kill = 12
+}
---
+++ Curiosity/Curiosity/Rover.cs
@@ -0,0 +1,67 @@
+using System.Text;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+
+public class Rover
+{
+    const string ServiceGuid = "0c0e1df3-0007-4d0e-9ff6-7c8eb9e98f2f";
+    const string CmdGuid = "0d58e717-57c0-4b0e-9723-c5e41e095897";
+    const string TmpGuid = "522bf40e-a020-4d92-b4c5-172fa16c667a";
+    IAdapter _adapter = CrossBluetoothLE.Current.Adapter;
+    ICharacteristic? _cmdCharacteristic;
+    ICharacteristic? _tmpCharacteristic;
+    public Rover()
+    {
+        _adapter.DeviceConnected += (s, a) =>
+        {
+            // Handle device connected event
+        };
+
+        _adapter.DeviceDisconnected += async (s, a) =>
+        {
+            await StartConnect();
+        };
+
+    }
+    public bool IsConnected => _adapter.ConnectedDevices.Count > 0;
+    public bool Enable { get; set; } = true;
+    public int Temperature { get; set; }
+    public Guid DeviceGuid { get; set; } = new Guid("00000000-0000-0000-0000-84cca860fefa");
+    private IDevice? _device;
+    public async Task ConnectAsync()
+    {
+        _device = await _adapter.ConnectToKnownDeviceAsync(DeviceGuid);
+        var service = await _device.GetServiceAsync(Guid.Parse(ServiceGuid));
+        _cmdCharacteristic = await service.GetCharacteristicAsync(Guid.Parse(CmdGuid));
+        _tmpCharacteristic = await service.GetCharacteristicAsync(Guid.Parse(TmpGuid));
+        _tmpCharacteristic.ValueUpdated += (s, a) =>
+        {
+
+            Text = Encoding.UTF8.GetString(a.Characteristic.Value);
+        };
+        await _tmpCharacteristic.StartUpdatesAsync();
+    }
+    public async Task StartConnect()
+    {
+        while (Enable && !IsConnected)
+        {
+            try
+            {
+                await ConnectAsync();
+            }
+            catch { }
+        }
+    }
+    public string Text { get; set; }
+    public async Task<bool> SendCommandAsync(Command command)
+    {
+        if (!IsConnected || _cmdCharacteristic == null)
+            return false;
+
+        var commandByte = (byte)command;
+        var commandArray = new byte[] { commandByte };
+        await _cmdCharacteristic.WriteAsync(commandArray);
+
+        return true;
+    }
+}
